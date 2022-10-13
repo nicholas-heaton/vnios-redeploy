4a33d148701f0cbab7205e59f04ad5884fedae0a
Nick Heaton <59534769+nicholas-heaton@users.noreply.github.com>
2022-10-12 21:19:35 -0400

    Update redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source venv/bin/activate
-ansible-playbook vnios_redeploy.yaml
+# ansible-playbook vnios_redeploy.yaml  #uncomment for ESXi
+# ansible-playbook vnios_redeploy_vcenter.yaml #uncomment for vCenter
